f99e83e5e10af81c86b0c1a127157c7194af123f
YING MA <37651084+maying0120@users.noreply.github.com>
2021-01-26 20:13:58 -0500

    Add files via upload

---
+++ 135Candy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        res = [1 for i in range(len(ratings))]  
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                res[i] =res[i-1]+1
+                
+        for j in range(len(ratings)-2,-1,-1):  
+    
+            if ratings[j] > ratings[j+1]:
+            #[1,3,4,5,2] 这种情况
+                res[j] = max(res[j+1]+1,res[j])
+                
+        print('res',res) 
+        return sum(res)
