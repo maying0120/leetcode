e2686ba6dd9bacec384814febfd2c771541f0134
YING MA <37651084+maying0120@users.noreply.github.com>
2021-01-29 16:09:36 -0500

    Update 340Longest Substring with At Most K Distinct Characters.py

--- 340Longest Substring with At Most K Distinct Characters.py
+++ 340Longest Substring with At Most K Distinct Characters.py
@@ -24,4 +24,36 @@ class Solution(object):
             res = max(res,right-left)
         return res
             
-                    
+#java
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        
+        int n= s.length();
+        if (n*k==0)
+        {
+            return 0;
+        }
+        int left=0;
+        int right =0;
+        Map<Character,Integer> map = new HashMap<>();
+        
+        int res=1;
+        while(right<n)
+        {
+            map.put(s.charAt(right),right);
+            right++;
+            
+            if (map.size()==k+1)
+            {
+                int lowidx = Collections.min(map.values());
+                map.remove(s.charAt(lowidx));
+                left = lowidx+1;
+                
+            }
+            res = Math.max(res,right-left);
+            
+        }
+        return res;
+        
+    }
+}
